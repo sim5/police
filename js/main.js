b34c63ef019747fc507f04fd48b164de1213200e
sim5 <sim5@gmu.edu>
2015-02-19 14:57:32 -0500

    update

--- js/main.js
+++ js/main.js
@@ -8,36 +8,26 @@ function preload() {
 game.load.audio('music','assets/scary.mp3');
 game.load.audio('jump','assets/jump.mp3');
 game.load.image('city', 'assets/jail.jpg');
-game.load.image('street', 'assets/street.png');
 game.load.image('ground','assets/platforms.png');    
-game.load.image('another-bg','assets/another_bg.png');    
-game.load.image('heart','assets/heart.png');
 game.load.image('spike','assets/spike.png');
 game.load.image('prisoner','assets/prisoner.png');
 game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-//game.load.spritesheet('cat', 'assets/Burgercat_main.png',32,40, 14);
-game.load.spritesheet('cat', 'assets/pol.png', 32,50 );
-game.load.spritesheet('baddie', 'assets/baddie.png', 32,32);
+game.load.spritesheet('police', 'assets/pol.png', 32,50 );
+game.load.spritesheet('mouse', 'assets/mouse.png', 50,50);
 
 }
 
 var player;
-var enemy;
 var keys = Phaser.Keyboard;
 var platforms;
-var cursors;
-var cats;
+var polices;
 var music;
 var jump;
 var world;
-var hearts;
-var enemy;
-var enemies;
-var scoreText;
-var score = 0;
-var levelExist = false;
-var isDead = false;
-var bgtile;
+var mouse;
+var mice;
+
+var background;
 var ground;
 var spikes;
 var trap;
@@ -52,12 +42,11 @@ function create(){
     music.play();
     jump = game.add.audio('jump');
     
-    //Physics time!
+
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    //The background
-    //Side scrolling background
-    bgtile = game.add.tileSprite(0,0, 9999, 597, 'city');
+
+    background = game.add.tileSprite(0,0, 2500, 597, 'city');
     
   stateText = game.add.text(game.world.centerX,game.world.centerY,' ', { font: '84px Arial', fill: '#fff' });
     stateText.anchor.setTo(0.5, 0.5);
@@ -72,74 +61,53 @@ function create(){
 					prisoner.body.immovable = true;
    
    
-    //adding platforms and it's physics
+   
     platforms = game.add.group();
     platforms.enableBody = true;
 	
 	spikes = game.add.group();
 	 spikes.enableBody = true;
    
-   /*
-    ground = platforms.create(0, game.world.height - 10, 'platform');
+  
+    map(platforms);
 
-    //Scale it to fit the game width and such
-    ground.scale.setTo(50,1);
-    ground.body.immovable = true;
-    */
-    //Ledge time
-   // makePlatforms();
-    part1(platforms);
-
-    //Making them players
- //   player = game.add.sprite(32, game.world.height - 150, 'dude');
-    player = game.add.sprite(32, game.world.height - 150, 'cat');
+    
+    player = game.add.sprite(32, game.world.height - 150, 'police');
     game.physics.arcade.enable(player);
    
     player.body.bounce.y = 0.2;
     player.body.gravity.y  = 400;
     player.body.collideWorldBounds = true;
     
-    //Adding their animations
+
 	  player.animations.add('stand', [0,1], 10, true);
     player.animations.add('left', [4, 5, 6, 7], 10, true);
     player.animations.add('right', [8, 9, 10, 11], 10, true);
 
-    
-    //Adding hearts
-    hearts = game.add.group();
-    hearts.enableBody = true;
+
 	
-
-
-    game.time.events.repeat(Phaser.Timer.SECOND*2, 500, createHearts, this); 
+  
+    mice = game.add.group();
+    createmouse();
     
 
-    //Adding enmies
-    enemies = game.add.group();
-    //enemies.enableBody = true;
-    createEnemy();
-    
-    scoreText = game.add.text(16, 16, 'Hearts: '+ score, { fontSize: '32px', fill: '#000'});
-
-    
-
-   // player.anchor.setTo(0.5,0.5);
     game.camera.follow(player);
-   game.time.events.loop(Phaser.Timer.SECOND, enemyMove, this); 
+   game.time.events.loop(Phaser.Timer.SECOND, mouseMove, this); 
 }
 
 function update(){
-    //Constantly update them game physics doe
+   
     game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.collide(hearts, platforms);
-    game.physics.arcade.collide(enemies, platforms);
+ 
+    game.physics.arcade.collide(mice, platforms);
 
-    //adding overlap to trigger functions when they overlap
-    game.physics.arcade.overlap(player, hearts, collectHearts, null, this);
-    game.physics.arcade.overlap(player, enemies, playerKill, null, this);
+  
+    game.physics.arcade.overlap(player, mice, playerKill, null, this);
 			    game.physics.arcade.overlap(player, prisonerBody, win, null, this);
 		
 			    game.physics.arcade.overlap(player, spikes, playerDie, null, this);
+				
+				
     
     player.body.velocity.x = 0;
 
@@ -160,8 +128,7 @@ function update(){
 	
 	player.frame = 6;
     }
-    
-    //Jump functions
+
     if (game.input.keyboard.isDown(keys.UP) && player.body.touching.down)
     {
 	jump.volume = 0.5;
@@ -176,92 +143,72 @@ player.reset(32, game.world.height - 150);
 
 
     }
+	
+	
     
 
-  /* enemies.forEach(function(enemy) {
-    var x = Math.round(Math.random());
-    if(x == 1)
-    {
-	if(enemy.body.touching.down)
-	{
-	    enemy.body.velocity.y = -400;
-	}
-	enemy.animations.play('left');
-	enemy.body.velocity.x = -150;
-	
-    }
-    if(x == 0)
-    {
-	if(enemy.body.touching.down)
-	{
-	    enemy.body.velocity.y = -400;
-	}
-	enemy.animations.play('right');
-	enemy.body.velocity.x = 150;
-    }
-   }, this);*///enemies.forEach(enemyMove,game.physics, false, 200);
-    updateScore(score);
+
 }
 
 function lockOnFollow() {
     game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON);
 }
 
-function part1(platforms) {
+function map(platforms) {
 
-//    bgtile = game.add.tileSprite(0,0, 1340, 597, 'city');
-    game.world.setBounds(0,0, 9999, 597);
 
-    var ledge = platforms.create(350, 400, 'ground');
-    ledge.body.immovable = true;
+    game.world.setBounds(0,0, 2500, 597);
+
+    var plat = platforms.create(350, 400, 'ground');
+    plat.body.immovable = true;
 	
 	trap = spikes.create(450, 350, 'spike');
         		trap.scale.setTo(.2, .2);
 					trap.body.immovable = true;
 	
-	    ledge = platforms.create(50, 500, 'ground');
-    ledge.body.immovable = true;
-	ledge.scale.setTo(.5,1);
+	    plat = platforms.create(50, 500, 'ground');
+    plat.body.immovable = true;
+	plat.scale.setTo(.5,1);
 	
 
 
-    ledge = platforms.create(650, 250, 'ground');
-    ledge.body.immovable = true;
+    plat = platforms.create(650, 250, 'ground');
+    plat.body.immovable = true;
 	
 		trap = spikes.create(750, 200, 'spike');
         		trap.scale.setTo(.2, .2);
 					trap.body.immovable = true;
 
-    ledge = platforms.create(200, 150, 'ground');
-    ledge.body.immovable = true;
+    plat = platforms.create(200, 150, 'ground');
+    plat.body.immovable = true;
 	
 		trap = spikes.create(350, 100, 'spike');
         		trap.scale.setTo(.2, .2);
 					trap.body.immovable = true;
     
-	var ledge = platforms.create(1100, 225, 'ground');
-    ledge.body.immovable = true;
+	var plat = platforms.create(1100, 225, 'ground');
+    plat.body.immovable = true;
 	
 		trap = spikes.create(1250, 175, 'spike');
         		trap.scale.setTo(.2, .2);
 					trap.body.immovable = true;
 
-    ledge = platforms.create(1400, 255, 'ground');
-    ledge.body.immovable = true;
+    plat = platforms.create(1400, 255, 'ground');
+    plat.body.immovable = true;
 	
 		trap = spikes.create(1500, 205, 'spike');
         		trap.scale.setTo(.2, .2);
 					trap.body.immovable = true;
 
-    ledge = platforms.create(1900, 350, 'ground');
-    ledge.body.immovable = true;
+    plat = platforms.create(1900, 350, 'ground');
+    plat.body.immovable = true;
 	
 		trap = spikes.create(2100, 300, 'spike');
         		trap.scale.setTo(.2, .2);
 					trap.body.immovable = true;
 	
-	    ledge = platforms.create(2000, 250, 'ground');
-    ledge.body.immovable = true;
+	    plat = platforms.create(2000, 250, 'ground');
+    plat.body.immovable = true;
 	
 		trap = spikes.create(2100, 200, 'spike');
         		trap.scale.setTo(.2, .2);
@@ -273,42 +220,30 @@ function part1(platforms) {
 
 
 
-function randomHeight() {
-    var width = 800;
-    return Math.random()*(width - (game.world.height - 150) + 150);
-}
 
-function createHearts() {
-    var y = randomHeight();
-    var heart = hearts.create(game.world.randomX, y, 'heart');
-    heart.body.gravity.y = 400;
-    heart.body.bounce.y = 0.8;
-}
 
-//Sprites of enemies
-function createEnemy() {
 
-    for (var i=0; i<6; i++) {
-	enemy = enemies.create(game.world.randomX, game.rnd.integerInRange(100, 600), 'baddie');
-	game.physics.arcade.enable(enemy);
-	enemy.body.gravity.y = 400;
-	enemy.body.bounce.y = 0.2;
-	enemy.animations.add('left',[0,1],10,true);
-	enemy.animations.add('right',[2, 3], 10, true);
-	enemy.body.collideWorldBounds = true;
+
+
+function createmouse() {
+
+    for (var i=0; i<10; i++) {
+	mouse = mice.create(game.rnd.integerInRange(200, 2000), game.rnd.integerInRange(100, 200), 'mouse');
+	game.physics.arcade.enable(mouse);
+	mouse.body.gravity.y = 300;
+	mouse.body.collideWorldBounds = true;
+	mouse.animations.add('left',[4,5,6,7],10,true);
+	mouse.animations.add('right',[8,9,10,11], 10, true);
+
 	}
 }
 
-function playerKill(player, enemies) {
+function playerKill(player, mice) {
 
-    score-=1;
-    updateScore(parseInt(score));
-    if(score <= 0) 
-    {
-	//player.kill();
-	score = 0;
+   
+   
 	player.reset(32, game.world.height - 150);
-    }
+    
 }
 
 function playerDie(player, spikes) {
@@ -328,45 +263,46 @@ function win(player, prisonerBody) {
 
 
 
-function enemyMove ()
+function mouseMove ()
     {
-	enemies.forEach(function(enemy) {
+	
+
+	mice.forEach(function(mouse) {
+	
+	 if (mouse.body.onFloor())
+    {
+mouse.kill();
+
+    }
     var x = Math.round(Math.random());
     if(x == 1)
     {
-	if(enemy.body.touching.down)
+	if(mouse.body.touching.down)
 	{
-	    enemy.body.velocity.y = -400;
+	    	mouse.animations.play('left');
+	mouse.body.velocity.x = -150;
 	}
-	enemy.animations.play('left');
-	enemy.body.velocity.x = -150;
+	
+	
+
 	
     }
 	    if(x == 0)
 	    {
-	if(enemy.body.touching.down)
+	if(mouse.body.touching.down)
 	{
-	    enemy.body.velocity.y = -400;
+	    mouse.animations.play('right');
+	mouse.body.velocity.x = 150;
 	}
-	enemy.animations.play('right');
-	enemy.body.velocity.x = 150;
+	
     }
 	}, this);
     }
 
 
 
-function collectHearts(player, hearts) {
-    
-    hearts.kill();
-    score+= 1;
-    updateScore(parseInt(score));
-
-}
 
 
 
-function updateScore(score) {
-    scoreText.setText("Hearts: " + score);
-}
+
 
